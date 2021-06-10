0ef73b9108b7b23cd823fedaf1a3ec5fe4a8efe2
Adeilson Aragão <adeilsonaragao@hotmail.com>
2021-06-10 10:12:45 -0300

    API REST - Criando a Classe Person

---
+++ PrimeiraAPIRest/APIRest01/APIRest01/Model/Person.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace APIRest01.Model {
+    public class Person {
+        public long Id { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string Endereco { get; set; }
+        public string Genero { get; set; }
+
+    }
+}
