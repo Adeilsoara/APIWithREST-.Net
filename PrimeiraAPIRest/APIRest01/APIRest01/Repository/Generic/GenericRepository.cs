b58ebf4b63614d7f66944e21fc091843e5ecdf06
Adeilson Aragão <adeilsonaragao@hotmail.com>
2021-06-12 09:45:19 -0300

    API REST - Implementação do Generic Repository

--- PrimeiraAPIRest/APIRest01/APIRest01/Repository/Generic/GenericRepository.cs
+++ PrimeiraAPIRest/APIRest01/APIRest01/Repository/Generic/GenericRepository.cs
@@ -1,31 +1,80 @@
 
 using APIRest01.Model.Base;
+using APIRest01.Model.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace APIRest01.Repository.Generic {
     public class GenericRepository<T> : IRepository<T> where T : BaseEntity {
+
+        private MysqlContext _context;
+        private DbSet<T> dataset;
+
+        public GenericRepository(MysqlContext context) {
+            _context = context;
+            dataset = _context.Set<T>();
+        }
+
         public T Create(T item) {
-            throw new System.NotImplementedException();
+            try {
+                dataset.Add(item);
+                _context.SaveChanges();
+                return item;
+            }
+            catch (Exception) {
+
+                throw;
+            }
+
         }
 
         public void Delete(long id) {
-            throw new System.NotImplementedException();
+            var result = dataset.SingleOrDefault(p => p.Id.Equals(id));
+            if (result != null) {
+                try {
+                    dataset.Remove(result);
+                    _context.SaveChanges();
+                }
+                catch (Exception) {
+
+                    throw;
+                }
+            }
         }
 
         public bool Exists(long id) {
-            throw new System.NotImplementedException();
+            return dataset.Any(p => p.Id.Equals(id));
         }
 
         public List<T> FindAll() {
-            throw new System.NotImplementedException();
+            return dataset.ToList();
         }
 
         public T FindById(long id) {
-            throw new System.NotImplementedException();
+            return dataset.SingleOrDefault(p => p.Id.Equals(id));
         }
 
         public T Update(T item) {
-            throw new System.NotImplementedException();
+
+            var result = dataset.SingleOrDefault(p => p.Id.Equals(item.Id));
+            if (result != null) {
+                try {
+                    _context.Entry(result).CurrentValues.SetValues(item);
+                    _context.SaveChanges();
+                    return result;
+
+                }
+                catch (Exception) {
+
+                    throw;
+                }
+            }
+            else {
+                return null;
+            }
         }
+
     }
 }
